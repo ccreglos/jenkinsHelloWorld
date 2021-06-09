cef56c2c4067d6333754216bdac17f7828984a49
Reglos, Christian <christian.reglos@accenture.com>
2021-06-09 21:40:32 +0800

    Adding new files

---
+++ CountWords.java
@@ -0,0 +1,10 @@
+public class CountWords {
+
+    public static void main(String[] args) {
+        String sentence = "Hello to Java programming!";
+        int count = 0;
+       
+        System.out.println("Sentence " + count);
+        System.out.println("Count " + sentence);
+    }
+}
